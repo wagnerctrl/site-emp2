d1d53b0c784c9a97a6fc05697dff13afba2fe737
wagnerctrl <wagnerh799@gmail.com>
2025-06-19 12:58:55 -0300

    upload

---
+++ website-enterprise-main/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
